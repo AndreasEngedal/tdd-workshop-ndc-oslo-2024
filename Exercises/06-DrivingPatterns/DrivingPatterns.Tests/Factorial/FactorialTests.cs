2b34680a6aea7a53770904019c3c4ed0c7264b73
Andreas Engedal <AGD@energinet.dk>
2024-06-10 11:38:21 +0200

    DrivingPatterns

---
+++ Exercises/06-DrivingPatterns/DrivingPatterns.Tests/Factorial/FactorialTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+
+namespace DrivingPatterns.Tests.Factorial;
+
+public class FactorialTests
+{
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 6)]
+    [InlineData(4, 24)]
+    [InlineData(5, 120)]
+    public void When_InputIsGiven_Then_FactorialIsReturned(int input, int expectedFactorial)
+    {
+        // Act
+        var actual = FactorialClass.Calculate(input);
+
+        // Assert
+        actual.Should().Be(expectedFactorial);
+    }
+}
+
+public static class FactorialClass
+{
+    public static int Calculate(int input)
+    {
+        if (input is 0 or 1)
+            return 1;
+
+        return input * Calculate(input - 1);
+    }
+}
---
+++ Exercises/06-DrivingPatterns/DrivingPatterns.Tests/SquareArea/SquareAreaTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+namespace DrivingPatterns.Tests.SquareArea;
+
+public class SquareAreaTests
+{
+    [Theory]
+    [InlineData(5, 25)]
+    [InlineData(4, 16)]
+    public void When_SideIsGiven_Expect_AreaIsReturned(int side, int expectedArea)
+    {
+        // Act
+        var area = SquareAreaClass.Calculate(side);
+
+        // Assert
+        area.Should().Be(expectedArea);
+    }
+}
+
+public class SquareAreaClass
+{
+    public static int Calculate(int side)
+    {
+        return side * side;
+    }
+}
